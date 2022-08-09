4033e526bf9e8188f98c7eea241a1923e7becbf3
saadSarwar28 <saad_sarwar22@hotmail.com>
2022-08-10 04:16:42 +0500

    all done ... phewww

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
+  images: {
+    loader: 'akamai',
+    path: '',
+  }
 }
 
 module.exports = nextConfig
