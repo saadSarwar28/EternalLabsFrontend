fa3159e23b054a9dad92dfda33cedc222373ace2
saadSarwar28 <saad_sarwar22@hotmail.com>
2022-11-05 01:39:31 +0500

    bug fix on build

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
                 <meta name="google-site-verification" content="mLdpfYdmDazUVMLZ9NWfUwH7wBnmMmtb374Hgz6ZiBk"/>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"/>
             </Head>
             <main className={styles.main}>
